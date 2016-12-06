90e737d71696a0385d1cdde121c7beb20456d7f5
SOK Pascal <p.sok@digi-lapa024.e-mazarine.home>
2016-12-06 09:47:16 +0100

    remove useless file

--- scripts/index.js
+++ scripts/index.js
@@ -4,8 +4,7 @@ import $ from 'chirashi';
 
 //var home = require('./home.js'); // CommonJS
 import home from './home'; // ES2015
-//import customButton from './custom-classes/custom-button.js';
 
 $.ready(() => {
-    console.log('index readyYY');
+    console.log('index ready');
 });
